d3759cedbe4625355a1112e99ece019347d733a9
Zandep <124667533+Zandep@users.noreply.github.com>
2023-04-11 12:12:25 +0400

    Update lab2.py

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,28 +1,32 @@
 import re
 
-number_to_words = {1: 'один', 2: 'два', 3: 'три', 4: 'четыре', 5: 'пять', 6: 'шесть', 7: 'семь', 8: 'восемь', 9: 'девять', 0: 'ноль'}
+number_to_words = {'+':'+','-':'-',',':',','.':'.','1': 'один', '2': 'два', '3': 'три', '4': 'четыре', '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять', '0': 'ноль'}
 number_counter = 0
+koll = 0
 
 try:
-    file = open("text.txt", "r")  # открываем файл
+    file = open("text.txt", "r")
     while True:
-        a = file.readline().split()  # читаем строку
-        #        print(a)
-        if not a:  # если файл пустой
+        a = file.readline().split()
+        if not a:
             print("\nФайл text.txt в директории проекта закончился.")
             break
         for j in a:
-            res = re.findall(r'(?<![\w/\;!?\\])\d+(?=[.,!?;])?(?!\w)', j)  # находим все натуральные числа с учетом грамматики
-            counter = 0  # счетчик для вывода
+            res = re.findall(r'[-+]?(?<![\w/\;!?\\])\d*[.,]\d*', j)
+            #print(''.join(res))
             number_counter += 1
-            if len(res) == 2 and int(res[0]) != 0:
-                for i in str(int(res[0])):  # вывод
-                    print(i,end="")
-                for g in str(int(res[1])):
-                    print(number_to_words[int(g)],end="")
+            if len(res) == 1 and ''.join(res[0]) != 0:
+                for i in ''.join(res[0]):
+                    if i == '.' or i == ',':
+                        koll += 1
+                    if koll == 1:
+                        print(number_to_words[str(i)], end=" ")
+                    else:
+                        print(i, end=" ")
                 print()
+                koll = 0
 
-    if number_counter == 0:  # если чисел, подходящих условию нет
+    if number_counter == 0:
         print()
         print('В файле нет чисел, удовлетворяющих условию. Добавьте числа в файл или переименуйте существующий *.txt файл.')
 

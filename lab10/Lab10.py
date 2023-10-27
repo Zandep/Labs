5c9b04a7fd2ef977d9dd073cd79ce584ff31be17
Zandep <124667533+Zandep@users.noreply.github.com>
2023-10-27 18:25:30 +0400

    Create Lab10.py

---
+++ lab10/Lab10.py
@@ -0,0 +1,137 @@
+from tkinter import *
+import random
+from tkinter.messagebox import showinfo
+
+root = Tk()
+root.title('Criss-cross')
+game_run = True
+field = []
+cross_count = 0
+
+
+
+def new_game():
+    for row in range(3):
+        for col in range(3):
+            field[row][col]['text'] = ' '
+            field[row][col]['background'] = 'lavender'
+    global game_run
+    game_run = True
+    global cross_count
+    cross_count = 0
+def click(row, col):
+    if game_run and field[row][col]['text'] == ' ':
+        field[row][col]['text'] = 'X'
+        global cross_count
+        cross_count += 1
+        check_win('X')
+        if game_run and cross_count < 5:
+            computer_move()
+            check_win('O')
+        if game_run and cross_count == 5:
+            showinfo(message="Ничья")
+
+def check_win(smb):
+    for n in range(3):
+        check_line(field[n][0], field[n][1], field[n][2], smb)
+        check_line(field[0][n], field[1][n], field[2][n], smb)
+    check_line(field[0][0], field[1][1], field[2][2], smb)
+    check_line(field[2][0], field[1][1], field[0][2], smb)
+
+def check_line(a1,a2,a3,smb):
+    if a1['text'] == 'X' and a2['text'] == 'X' and a3['text'] == 'X':
+        a1['background'] = a2['background'] = a3['background'] = 'red'
+        global game_run
+        showinfo(message="Вы выйграли")
+        game_run = False
+    if a1['text'] == 'O' and a2['text'] == 'O' and a3['text'] == 'O':
+        a1['background'] = a2['background'] = a3['background'] = 'pink'
+        showinfo(message="Вы проиграли")
+        game_run = False
+def can_win(a1,a2,a3,smb):
+    res = False
+    if a1['text'] == smb and a2['text'] == smb and a3['text'] == ' ':
+        a3['text'] = 'O'
+        res = True
+    if a1['text'] == smb and a2['text'] == ' ' and a3['text'] == smb:
+        a2['text'] = 'O'
+        res = True
+    if a1['text'] == ' ' and a2['text'] == smb and a3['text'] == smb:
+        a1['text'] = 'O'
+        res = True
+    return res
+
+def computer_move():
+    for n in range(3):
+        if can_win(field[n][0], field[n][1], field[n][2], 'O'):
+            return
+        if can_win(field[0][n], field[1][n], field[2][n], 'O'):
+            return
+    if can_win(field[0][0], field[1][1], field[2][2], 'O'):
+        return
+    if can_win(field[2][0], field[1][1], field[0][2], 'O'):
+        return
+    for n in range(3):
+        if can_win(field[n][0], field[n][1], field[n][2], 'X'):
+            return
+        if can_win(field[0][n], field[1][n], field[2][n], 'X'):
+            return
+    if can_win(field[0][0], field[1][1], field[2][2], 'X'):
+        return
+    if can_win(field[2][0], field[1][1], field[0][2], 'X'):
+        return
+    while True:
+        row = random.randint(0, 2)
+        col = random.randint(0, 2)
+        if field[row][col]['text'] == ' ':
+            field[row][col]['text'] = 'O'
+            break
+
+
+def evaluate(board_copy):
+    if check_win(board_copy, "X"):
+        return -1
+    elif check_win(board_copy, "O"):
+        return 1
+    elif " " not in board_copy:
+        return 0
+    else:
+        return None
+def minimax(board_copy, depth, is_max):
+    score = evaluate(board_copy)
+
+    if score is not None:
+        return score
+
+    if is_max:
+        best_score = -float("inf")
+        for i in range(9):
+            if board_copy[i] == " ":
+                board_copy[i] = "O"
+                score = minimax(board_copy, depth + 1, False)
+                board_copy[i] = " "
+                best_score = max(score, best_score)
+        return best_score
+    else:
+        best_score = float("inf")
+        for i in range(9):
+            if board_copy[i] == " ":
+                board_copy[i] = "X"
+                score = minimax(board_copy, depth + 1, True)
+                board_copy[i] = " "
+                best_score = min(score, best_score)
+        return best_score
+
+for row in range(3):
+    line = []
+    for col in range(3):
+        button = Button(root, text=' ', width=4, height=2,
+                        font=('Verdana', 20, 'bold'),
+                        background='lavender',
+                        command=lambda row=row, col=col: click(row,col))
+        button.grid(row=row, column=col, sticky='nsew')
+        line.append(button)
+    field.append(line)
+new_button = Button(root, text='new game', command=new_game)
+new_button.grid(row=3, column=0, columnspan=3, sticky='nsew')
+root.mainloop()

ee4c00b0f11321eebc0f9d4931a400f3ecc733c0
Zandep <124667533+Zandep@users.noreply.github.com>
2023-12-22 18:22:34 +0400

    Update Lab13.py

--- Lab13/Lab13.py
+++ Lab13/Lab13.py
@@ -4,15 +4,35 @@ import csv
 file = open("titanic.csv", "r")
 reader = list(csv.reader(file))
 
-det = []
-vzros = []
+det1 = []
+vzros1 = []
+det2 = []
+vzros2 = []
+det3 = []
+vzros3 = []
 for i in reader:
     if i[0] == "1":
-        if i[1] == "1" or i[1] == "2" or i[1] == "3":
+        if i[1] == "1":
             if float(i[4]) < 18.0:
-                det.append([i[4]])
+                det1.append([i[4]])
             else:
-                vzros.append([i[4]])
-
-print(f"Детей выжило:", len(det))
-print(f"Взрослых выжило:", len(vzros))
+                vzros1.append([i[4]])
+        if i[1] == "2":
+            if float(i[4]) < 18.0:
+                det2.append([i[4]])
+            else:
+                vzros2.append([i[4]])
+        if i[1] == "3":
+            if float(i[4]) < 18.0:
+                det3.append([i[4]])
+            else:
+                vzros3.append([i[4]])
+print("Выжившие в первом классе")
+print(f"Дети:", len(det1))
+print(f"Взрослые:", len(vzros1))
+print("Выжившие во втором классе")
+print(f"Дети:", len(det2))
+print(f"Взрослые:", len(vzros2))
+print("Выжившие в третьем классе")
+print(f"Дети:", len(det3))
+print(f"Взрослые:", len(vzros3))
